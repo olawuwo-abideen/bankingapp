47492e378b26ed672e40d02ebb1d8ec7c731f37f
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-03-29 21:30:30 +0100

    update  user microservices

---
+++ user-microservice/src/express-app.ts
@@ -0,0 +1,13 @@
+import express, { Express } from "express";
+import cors from "cors";
+import { account } from "./api";
+import { CreateChannel } from "./utils";
+
+export default async (app: Express) => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.static(__dirname + "/public"));
+  const channel = await CreateChannel();
+
+  user(app, channel);
+};
