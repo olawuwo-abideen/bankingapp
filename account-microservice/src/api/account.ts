9c36c019a35a931822c92b80470c71a34889f0f7
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-04-03 01:36:16 +0100

    update user microservices

--- account-microservice/src/api/account.ts
+++ account-microservice/src/api/account.ts
@@ -8,49 +8,25 @@ export default (app: any, channel: any) => {
     
     const service = new AccountService();
     
-    app.post("/signup", async (req: Request, res: Response, next: NextFunction) => {
+    app.post("/register", async (req: Request, res: Response, next: NextFunction) => {
         try {
-          const { email, password, phone } = req.body;
-          const data = await service.SignUp({ email, password, phone });
-          return res.json(data);
-        } catch (error) {
-          next(error);
-        }
-    });
-    
-    app.post("/login", async (req: Request, res: Response, next: NextFunction) => {
-        try {
-          const { email, password } = req.body;
-          const data = await service.SignIn({ email, password });
-          return res.json(data);
         } catch (error) {
           next(error);
         }
     });
 
-    app.post("/address", UserAuth, async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const { _id } = req.user;
-            const { street, postalCode, city, country } = req.body;
-            const data = await service.AddNewAddress(_id, {
-                street,
-                postalCode,
-                city,
-                country,
-            });
-            return res.json(data);
-        } catch (error) {
-            next(error);
-        }
-    });
-    
-    app.get("/profile", UserAuth, async (req: Request, res: Response, next: NextFunction) => {
-        try {
-          const { _id } = req.user;
-          const data = await service.GetProfile({ _id });
-          return res.json(data);
-        } catch (error) {
-          next(error);
-        }
-    });
-}
+    app.post("/login", async (req: Request, res: Response, next: NextFunction) => {
+      try {
+      } catch (error) {
+        next(error);
+      }
+  });
+  app.post("/logout", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+    } catch (error) {
+      next(error);
+    }
+});
+
+
+  }
