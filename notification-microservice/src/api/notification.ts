9a9e3ec415729872f2961a0e4efc558723fe43c2
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-04-05 01:50:22 +0100

    update notification services

---
+++ notification-microservice/src/api/notification.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import { NotificationService } from '../services/notification-service';
+import { UserAuth } from './middlewares/auth';
+import { SubscribeMessage } from '../utils';
+
+
+export default (app: any, channel: any) => {
+    
+    const service = new NotificationService();
+    
+    app.post("/notification/sendEmail", async (req: Request, res: Response, next: NextFunction) => {
+        try {
+        } catch (error) {
+          next(error);
+        }
+    }); 
+
+  }
