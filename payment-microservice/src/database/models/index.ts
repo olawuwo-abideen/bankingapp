154db075526f8a45c4a73ee79e83865b289292bc
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-04-28 16:39:57 +0100

    update payment microservices

--- payment-microservice/src/database/models/index.ts
+++ payment-microservice/src/database/models/index.ts
@@ -1,9 +1,17 @@
-import PaymentModel from './payment';
 import AirtimeModel from './airtime';
+import CableTVModel from './cabletv';
+import DataPurchaseModel from './datapurchase';
+import PaymentModel from './payment';
+import UtilityModel from './utility';
+
+
 
 
 export const models = {
+    AirtimeModel,
+    CableTVModel,
+    DataPurchaseModel,
     PaymentModel,
-    AirtimeModel 
+    UtilityModel,
 
 };
